d6fccb212f6c39dda627705a5ff5d2dfb74659ef
Jose Wellington <josewellingtonjunior@hotmail.com>
2021-07-29 15:29:08 -0300

    NavbarDropdownResponsive

--- src/pages/Classroom.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Classroom (){
-  return (
-    <div>
-      <h1>Classroom</h1>
-    </div>
-  );
-};
--- src/pages/Prist.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Prist (){
-  return (
-    <div>
-      <h1>Prist</h1>
-    </div>
-  );
-};
